93fde565b062efbbd414062f06fbe590807e939b
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-09-17 22:09:27 +0300

    DEMO -- add DeleteNetworkInterface

--- iam_policy_documents.tf
+++ iam_policy_documents.tf
@@ -38,7 +38,7 @@ data "aws_iam_policy_document" "demo_lambda_put_metric_policy_doc" {
 */
 data "aws_iam_policy_document" "demo_lambda_create_network_interface_policy_doc" {
   statement {
-    actions   = ["ec2:CreateNetworkInterface", "ec2:DescribeNetworkInterfaces"]
+    actions   = ["ec2:CreateNetworkInterface", "ec2:DescribeNetworkInterfaces", "DeleteNetworkInterface"]
     resources = ["*"]
     effect    = "Allow"
   }
