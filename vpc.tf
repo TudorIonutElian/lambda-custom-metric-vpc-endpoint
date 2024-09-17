d6d71a9ea1cac1d8d51bd8e8253203b54ccd81e3
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-09-17 21:14:03 +0300

    DEMO -- add docs for private vpc

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,10 @@
+/**
+ * Create a VPC with DNS support and DNS hostnames enabled
+  - The VPC will have a CIDR block of 10.0.0.0/16
+  - The VPC will have DNS support enabled
+  - The VPC will have DNS hostnames enabled
+  - The VPC will have a tag with the key Name and the value demo_private_vpc
+ */
 resource "aws_vpc" "demo_private_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
